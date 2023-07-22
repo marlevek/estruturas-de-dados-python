b2b5f0f43ea11b64dcfe5297d4580dc43ad8da09
Marcelo <marcelo_levek@hotmail.com>
2023-07-22 13:44:43 -0300

    modificados os exemplos

--- 01_Listas/01_declaracao_listas.py
+++ 01_Listas/01_declaracao_listas.py
@@ -1,5 +1,7 @@
-frutas = ['laranja', 'abacate', 'morango', 'goiaba']
-print(frutas)
+frutas1 = ['laranja', 'abacate', 'morango', 'goiaba']
+print(frutas1)
+print(frutas1[1])
+print(frutas1[-2])
 
 frutas = []
 print(frutas)
@@ -11,5 +13,5 @@ numeros = list(range(10))
 print(numeros)
 
 carro = ['ferrari', 'F50', 4200000, 2020, 2900, 'Curitiba', True]
-
 print(carro)
+
