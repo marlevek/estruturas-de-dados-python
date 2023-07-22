b90777bdea0ec1f28a488246d13f1b7491231ea9
Marcelo <marcelo_levek@hotmail.com>
2023-07-22 13:40:18 -0300

    Inserindo a pasta Listas

---
+++ 01_Listas/01_declaracao_listas.py
@@ -0,0 +1,15 @@
+frutas = ['laranja', 'abacate', 'morango', 'goiaba']
+print(frutas)
+
+frutas = []
+print(frutas)
+
+letras = list('python')
+print(letras)
+
+numeros = list(range(10))
+print(numeros)
+
+carro = ['ferrari', 'F50', 4200000, 2020, 2900, 'Curitiba', True]
+
+print(carro)
